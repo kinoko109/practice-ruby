e42456d2afa9d2ca9e1bfae5b0fe529d143dd61d
kinoko109 <tatsuaki1009@gmail.com>
2024-10-15 22:19:58 +0900

    コメント

--- 7kyu/54ba84be607a92aa900000f1.rb
+++ 7kyu/54ba84be607a92aa900000f1.rb
@@ -1,5 +1,7 @@
 # https://www.codewars.com/kata/54ba84be607a92aa900000f1/solutions
 def is_isogram(string)
+  # .chars→文字列を個別の文字列の配列に変換
+  # .uniq→ユニークな要素のみを残す
   string.downcase.chars.uniq == string.downcase.chars
 end
 
