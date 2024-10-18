0a43dec66c547915e9afff880686598755e21a15
kinoko109 <tatsuaki1009@gmail.com>
2024-10-18 21:15:34 +0900

    Are You Playing Banjo?

---
+++ 8kyu/53af2b8861023f1d88000832.rb
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/53af2b8861023f1d88000832/ruby
+def are_you_playing_banjo(name)
+  # Implement me!
+  name.start_with?("r", "R") ? name + " plays banjo" : name + " does not play banjo"
+end
+
+def are_you_playing_banjo(name)
+  name[0].downcase == "r" ? "#{name} plays banjo" : "#{name} does not play banjo"
+end
+
+def are_you_playing_banjo(name)
+  "#{name} #{name =~ /^r/i ? 'plays' : 'does not play'} banjo"
+end
