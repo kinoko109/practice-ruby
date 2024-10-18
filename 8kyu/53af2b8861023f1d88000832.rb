985946362bb27808964240a3e72464bbb42a3bd8
kinoko109 <tatsuaki1009@gmail.com>
2024-10-18 21:17:01 +0900

    コメント

--- 8kyu/53af2b8861023f1d88000832.rb
+++ 8kyu/53af2b8861023f1d88000832.rb
@@ -1,6 +1,7 @@
 # https://www.codewars.com/kata/53af2b8861023f1d88000832/ruby
 def are_you_playing_banjo(name)
   # Implement me!
+  # 先頭が prefixes のいずれかであるとき true を返す
   name.start_with?("r", "R") ? name + " plays banjo" : name + " does not play banjo"
 end
 
